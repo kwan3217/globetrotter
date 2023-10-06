01cdcc3030986a01e0b4b410dd4bab46d31790e8
kwan3217 <kwan3217@gmail.com>
2023-10-06 10:23:13 -0600

    Refactored to:
    * Commit to Postgres
    * Use remote database host in Postgres (so data can be stored on Pinkiepie)
    * Use a different schema for each trip

--- import_ubx.py
+++ import_ubx.py
@@ -3,6 +3,7 @@ Import gpsd pseudo-NMEA, which are created from the input binary datastream (in 
 """
 import bz2
 import gzip
+import warnings
 from glob import glob
 from os.path import basename
 
@@ -13,7 +14,8 @@ from database.postgres import PostgresDatabase
 from packet.ublox.protocol_33_21 import packet_names, UBX_NAV_PVT, UBX_NAV_TIMEGPS, UBX_NAV_EOE, UBX_ESF_MEAS
 from packet.ublox import ensure_tables, read_packet
 from packet import register_file_start, register_file_finish, ensure_timeseries_tables, register_epoch
-
+import packet.json # Don't use anything, just register it
+import packet.nmea # likewise
 
 def smart_open(fn,mode:str=None):
     if ".bz2" in fn:
@@ -53,14 +55,18 @@ def handle_packet(db:Database, fileid:int, ofs:int, packet:'Packet')->None:
             (hasattr(handle_packet,'week') and handle_packet.week is not None)):
             if handle_packet.iTOW != packet.iTOW:
                 raise ValueError(f"Unexpected packet iTOW: Expected {handle_packet.iTOW}, saw {packet.iTOW}, "
-                                 f"packet type {packet.__class__.name()}")
+                                 f"packet type {packet.__class__.__name__}")
             epochid,pre_exist=register_epoch(db,utc=handle_packet.utc, iTOW=handle_packet.iTOW, week=handle_packet.week)
             if not pre_exist:
+                write_epoch=True
                 for epoch_packet in handle_packet.epoch_packets:
                     if hasattr(epoch_packet,'iTOW') and handle_packet.iTOW!=epoch_packet.iTOW:
-                        raise ValueError("Packet has iTOW that doesn't match epoch: Expected {handle_packet.iTOW}, "
-                                         f"saw {epoch_packet.iTOW}, packet type {packet.__class__.name()}")
-                    epoch_packet.write(db,epochid=epochid,fileid=epoch_packet.fileid,ofs=epoch_packet.ofs)
+                        warnings.warn(f"Packet has iTOW that doesn't match epoch: Expected {handle_packet.iTOW}, "
+                                      f"saw {epoch_packet.iTOW}, packet type {packet.__class__.__name__}")
+                        write_epoch=False
+                if write_epoch:
+                    for epoch_packet in handle_packet.epoch_packets:
+                        epoch_packet.write(db,epochid=epochid,fileid=epoch_packet.fileid,ofs=epoch_packet.ofs)
         else:
             print("Incomplete epoch")
         handle_packet.epoch_packets=[]
@@ -92,19 +98,49 @@ def plot_height(db):
     plt.pause(1)
 
 
-def main():
-    dbname="Atlantic23_05"
-    import_files=True
-    do_plot=True
-    drop=False
-    profile=False
-    with PostgresDatabase(user="jeppesen", password="Locking1blitz", database=dbname) as db:
+def plot_speed(db):
+    sql = 'select utc,hMSL,veln,vele,veld from nav_pvt where gnssfixok order by utc;'
+    db.execute(sql)
+    utcs, hMSLs,velns, veles, velds = [], [], [], [], []
+    for utc, hMSL, veln, vele, veld in db._cur:
+        utcs.append(utc)
+        hMSLs.append(hMSL)
+        velns.append(veln)
+        veles.append(vele)
+        velds.append(veld)
+    print(utcs[0])
+    velns=np.array(velns)
+    veles=np.array(veles)
+    velds=np.array(velds)
+    vels=np.sqrt(velns**2+veles**2+velds**2)
+    plt.figure("height,speed")
+    plt.clf()
+    plt.subplot(121)
+    plt.plot(utcs, hMSLs)
+    plt.xlabel("UTC")
+    plt.ylabel("hMSL/m")
+    plt.subplot(122)
+    plt.plot(utcs, vels)
+    plt.xlabel("UTC")
+    plt.ylabel("vel/(m/s)")
+    plt.pause(1)
+
+
+def import_ubx(*,infns:list[str],dbname:str="globetrotter",schema:str,
+               host:str="192.168.217.102",port:int=5432,user:str="globetrotter",password:str="globetrotter",
+               import_files:bool=True,do_plot:bool=True,drop:bool=True,profile:bool=False):
+    n_pvt = 0
+    with PostgresDatabase(host=host,port=port,user=user, password=password, database=dbname) as db:
+        with db.transaction():
+            if drop:
+                db.execute(f"DROP SCHEMA IF EXISTS {schema} CASCADE;")
+            db.execute(f"CREATE SCHEMA IF NOT EXISTS {schema};")
+            db.execute(f"SET SEARCH_PATH={schema};")
         if import_files:
             with db.transaction():
                 ensure_timeseries_tables(db,drop=drop)
                 ensure_tables(db,drop=drop)
             seen_clsids={}
-            infns=sorted(glob('/mnt/big/Atlantic23.05/Fluttershy/FluttershyBase/2023/05/13/*.ubx.bz2'))[0:1]
             for infn in infns:
                 with db.transaction():
                     fileid=register_file_start(db,basename(infn))
@@ -127,7 +163,10 @@ def main():
                                     seen_clsids[clsid]=[True,0]
                                 seen_clsids[clsid][1]+=1
                                 if type(packet)==UBX_NAV_PVT:
-                                    print(packet.utc)
+                                    print('.',end='')
+                                    n_pvt+=1
+                                    if n_pvt%100==0:
+                                        print(packet.utc)
                                 handle_packet(db,fileid,this_ofs,packet)
                                 this_ofs=next_ofs
                             elif type(packet)==packet.ublox_packet.UBloxPacket:
@@ -139,7 +178,7 @@ def main():
                 with db.transaction():
                     register_file_finish(db, fileid)
                 if do_plot:
-                    plot_height(db)
+                    plot_speed(db)
             k=sorted(seen_clsids.keys())
             for cls,id in k:
                 if cls in packet_names:
@@ -152,7 +191,10 @@ def main():
                     idname=f"0x{id:02x}"
                 n=seen_clsids[(cls,id)]
                 print(f"{clsname}-{idname} (0x{cls:02x},0x{id:02x}): {n}")
-            print(register_epoch.now-register_epoch.first)
+            try:
+                print(register_epoch.now-register_epoch.first)
+            except AttributeError:
+                print("Couldn't print register_epoch.now (no packets processed?)")
             if profile:
                 d = {k: (v[0], v[1]) for k, v in dict(sorted(db.profile.items(), key=lambda item: -item[1][1])).items()}
                 print(d)
@@ -167,6 +209,25 @@ def main():
         if do_plot:
             plot_height(db)
             plt.show()
+
+
+def main():
+    case="Bahamas22_08"
+    if case=="Colorado23_09":
+        infns=sorted(
+            #Colorado 23.09 debug case
+            #glob('/mnt/big/Colorado23.09/FluttershyBase/2023/09/27/*00-40-01.ubx', recursive=True)
+            #Colorado 23.09 all
+            glob('/mnt/big/Colorado23.09/FluttershyBase/2023/09/2[789]/*.ubx.bz2', recursive=True) +
+            glob('/mnt/big/Colorado23.09/FluttershyBase/2023/09/2[789]/*.ubx', recursive=True)
+        )
+    elif case=="Atlantic23_05":
+        infns = sorted(glob('/mnt/big/Atlantic23.05/Fluttershy/FluttershyBase/2023/05/0[789]/*.ubx.bz2')+
+                       glob('/mnt/big/Atlantic23.05/Fluttershy/FluttershyBase/2023/05/20/*.ubx.bz2'))
+    elif case=="Bahamas22_08":
+        infns = sorted(glob('/mnt/big/Bahamas22.08/FluttershyBase/**/*.ubx.bz2',recursive=True)+
+                       glob('/mnt/big/Bahamas22.08/FluttershyBase/**/*.ubx',recursive=True))
+    import_ubx(infns=infns,schema=case)
 
 if __name__=="__main__":
     main()
